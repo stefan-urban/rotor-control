b1ae7dc5d4196e1cf521a7a1dd5478916ad5a406
Stefan Urban <stefan.urban@live.de>
2014-06-24 14:24:18 +0200

    Added speed dummy commands to GS-232 interface

--- software/gs232-interface.c
+++ software/gs232-interface.c
@@ -19,9 +19,25 @@
 
 char* gs232_command(char *cmd_str) {
 
+	// Set speed to "Low"
+	if (strcmp(cmd_str, "X1" HAMLIB_EOM) == 0) {
+		fprintf(stdout, "Set speed 1\n");
+		fflush(stdout);
+
+		return "ok"; // HAMLIB_REPLY_EOM;
+	}
+
+	// Set speed to "Middle 1"
+	if (strcmp(cmd_str, "X2" HAMLIB_EOM) == 0) {
+		fprintf(stdout, "Set speed 2\n");
+		fflush(stdout);
+
+		return "ok"; // HAMLIB_REPLY_EOM;
+	}
+
 	// Get the current rotor position
 	if (strcmp(cmd_str, "C2" HAMLIB_EOM) == 0) {
-		fprintf(stdout, "Get the current rotor position");
+		fprintf(stdout, "Get the current rotor position\n");
 		fflush(stdout);
 
 		return "AZ=123   EL=010"; // HAMLIB_REPLY_EOM;
@@ -29,13 +45,13 @@ char* gs232_command(char *cmd_str) {
 
 	// Stop
 	if (strcmp(cmd_str, "S" HAMLIB_EOM) == 0) {
-		fprintf(stdout, "Stopping rotor control!");
+		fprintf(stdout, "Stopping rotor control!\n");
 		fflush(stdout);
 
 		return "?" HAMLIB_REPLY_EOM;
 	}
 
-	fprintf(stdout, "Did not understand %s", cmd_str);
+	fprintf(stdout, "Did not understand %s\n", cmd_str);
 	fflush(stdout);
 
 	return "?" HAMLIB_REPLY_EOM;
