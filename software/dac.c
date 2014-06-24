12ea640977bc2de1744f2259a59ffb2abd481e28
Stefan Urban <stefan.urban@live.de>
2014-06-24 10:05:32 +0200

    First draft for DAC interface

---
+++ software/dac.c
@@ -0,0 +1,46 @@
+/*
+ * dac.c
+ *
+ *  Created on: 26.06.2014
+ *      Author: Stefan Urban
+ */
+
+#include <stdint.h>
+
+
+void dac_init(dac_t dac)
+{
+	(*(dac.init_function))();
+}
+
+void dac_enable(dac_t dac)
+{
+	(*(dac.enable_function))();
+}
+
+void dac_disable(dac_t dac)
+{
+	(*(dac.disable_function))();
+}
+
+void dac_set_voltage(dac_t dac, uint16_t new_voltage)
+{
+	uint16_t new_dac_value;
+	
+	// new_dac_value = (new_voltage / reference_voltage) * 2 ^ resolution;
+	uint32_t tmp = new_voltage << dac.resolution;
+	new_dav_value = (uint16_t) (tmp / reference_voltage);
+	
+	(*(dac.set_value_function))(new_dac_value);
+}
+
+uint16_t dac_get_voltage(dac_t dac)
+{
+	uint16_t voltage;
+	
+	// voltage = dac_value / (2^resolution) * reference_voltage
+	uint32_t tmp = (*(dac.get_value_function))() * dac.reference_voltage;
+	voltage = tmp >> resolution;
+	
+	return voltage;
+}
---
+++ software/ti-tlv5616.h
@@ -0,0 +1,46 @@
+/*
+ * ti-tlv5616.h
+ *
+ *  Created on: 24.06.2014
+ *      Author: Stefan Urban
+ */
+
+#ifndef TI_TLV5616_H_
+#define TI_TLV5616_H_
+
+#include <stdint.h>
+
+#include "dac.h"
+
+
+/**
+ * DAC device representation for azimuth and elevation rotor
+ */
+extern dac_t rot_a_dac;
+extern dac_t rot_e_dac;
+
+/**
+ * Initialize chip, bus leaves it disabled, call tlv5616_enable() before using
+ */
+void tlv5616_init(void);
+
+/**
+ * Enable output
+ */
+ void tlv5616_enable(void);
+
+/**
+ * Disable output
+ */
+void tlv5616_disable(void);
+
+/**
+ * Set the new output value
+ *
+ * @param new_dac_value: 12-bit!
+ */
+void tlv5616_set_value(uint16_t new_dac_value);
+
+
+
+#endif /* TI_TLV5616_H_ */
