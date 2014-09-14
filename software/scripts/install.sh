ff54e6898c275605f26ed1958425af8d0f65a63b
Stefan Urban <stefan.urban@live.de>
2014-09-14 17:17:11 +0200

    Tweaked installation script

--- software/scripts/install.sh
+++ software/scripts/install.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
+CONFIG_FILE=/etc/rotor_control.conf
+DIRECTORY=/opt/rotor-control
+
 
 printf " ------------------------------------------------------------------------------------------------- \n"
-printf "  Building software!"
+printf "  Building software ..."
 printf "\n"
 
 # Build everything
 cd ../src
 make > /dev/null
 
-printf "       ... Finished!\n"
+if [ -f rotor-control ];
+then
+	printf "       ... Finished!\n"
+else
+	printf "       ... Build failed!\n"
+	exit
+fi
 
 printf " ------------------------------------------------------------------------------------------------- \n"
-printf "  Copy executable to new location!\n"
+printf "  Copy executable to /opt/rotor-control/rotor-control ...\n"
 
 # Copy executable to new location
-DIRECTORY=/opt/rotor-control
 if [ -d $DIRECTORY ];
 then 
 	printf "       Directory $DIRECTORY already exists!\n"
@@ -24,43 +32,97 @@ else
 	printf "       Directory $DIRECTORY created!\n"
 fi
 
-if [ -d $DIRECTORY ];
+if [ -f $DIRECTORY/rotor-control ];
 then 
 	while true; do
 	    read -p "       Executable already exists. Do you wish to overwrite it? (y/n) " yn
 	    case $yn in
-		[Yy]* ) break;;
-		[Nn]* ) exit;;
+		[Yy]* )
+                    cp rotor-control $DIRECTORY/
+                    printf "       Executable copied!\n"
+                    break;;
+		[Nn]* ) break;;
 		* ) echo "Please answer yes or no.";;
 	    esac
 	done
 else
-	mkdir $DIRECTORY
+	cp rotor-control $DIRECTORY/
+	printf "       Executable copied!\n"
 fi
 
-cp rotor-control $DIRECTORY/
-printf "       Executable copied!\n"
 
 
 printf " ------------------------------------------------------------------------------------------------- \n"
-printf "  Copy start/stop scripts to new location!\n"
+printf "  Copy start/stop scripts to /opt/rotor-control/ ...\n"
 
-sudo cp ../scripts/start-rotor-control.sh $DIRECTORY
-sudo cp ../scripts/stop-rotor-control.sh $DIRECTORY
+if [ -f $DIRECTORY/start-rotor-control.sh ];
+then 
+	while true; do
+	    read -p "       Start/Stop scripts already exist. Do you wish to overwrite them? (y/n) " yn
+	    case $yn in
+		[Yy]* )
+                    sudo cp ../scripts/start-rotor-control.sh $DIRECTORY
+                    sudo cp ../scripts/stop-rotor-control.sh $DIRECTORY
+                    printf "       ... Scripts copied!\n"
+                    break;;
+		[Nn]* ) break;;
+		* ) echo "Please answer yes or no.";;
+	    esac
+	done
+else
+	sudo cp ../scripts/start-rotor-control.sh $DIRECTORY
+	sudo cp ../scripts/stop-rotor-control.sh $DIRECTORY
+	printf "       ... Scripts copied!\n"
+fi
+
+
+printf " ------------------------------------------------------------------------------------------------- \n"
+printf "  Install daemon script to /etc/init.d/rotor-control ...\n"
+
+if [ -f $DIRECTORY/rotor-control ];
+then 
+	while true; do
+	    read -p "       Daemon script already exists. Do you wish to overwrite it? (y/n) " yn
+	    case $yn in
+		[Yy]* )
+                    sudo cp ../scripts/rotor-control /etc/init.d/rotor-control
+                    printf "       Daemon script installed!\n"
+                    break;;
+		[Nn]* ) break;;
+		* ) echo "Please answer yes or no.";;
+	    esac
+	done
+else
+	sudo cp ../scripts/rotor-control /etc/init.d/rotor-control
+	printf "       Daemon script installed!\n"
+fi
+
+
+
 
 printf "       ... Finished!\n"
-
 printf " ------------------------------------------------------------------------------------------------- \n"
-printf "  Install rotor-control daemon!\n"
+printf "  Install configuration file $CONFIG_FILE ...\n"
 
-sudo cp ../scripts/rotor-control /etc/init.d/rotor-control
 
-printf "       ... Finished!"
-printf " ------------------------------------------------------------------------------------------------- \n"
-printf "  Install configuration file!\n"
+if [ -f $CONFIG_FILE ];
+then 
+	while true; do
+	    read -p "       Configuration file already exists. Do you wish to overwrite it? (y/n) " yn
+	    case $yn in
+		[Yy]* )
+                    sudo cp ../docs/rotor_control.conf.sample $CONFIG_FILE
+                    printf "       ... Configuration file copied!\n"
+                    break;;
+		[Nn]* ) break;;
+		* ) echo "Please answer yes or no.";;
+	    esac
+	done
+else
+	sudo cp ../docs/rotor_control.conf.sample $CONFIG_FILE
+	printf "       ... Configuration file copied!\n"
+fi
 
-sudo cp ../docs/rotor_control.conf.sample /etc/rotor_control.conf.sample
 
-printf "       ... Finished!"
 
-printf "\n\n"
+printf "\n"
